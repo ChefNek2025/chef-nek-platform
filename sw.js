83d7f54b4ae4dd01fe290ff3a9ff8cfcd343feca
ChefNek2025 <hailu.nekatibeb2025@gmail.com>
2026-03-30 19:28:32 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'chef-nek-pro-v1';
+const ASSETS = ['/'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() =>
+      caches.match(e.request)
+    )
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
